eee42fdfdafa868694b7a435570c65fe5f98c627
Mutian Li <57753175+leinace1001@users.noreply.github.com>
2025-05-09 11:22:50 -0400

    Add files via upload

--- transcribe.py
+++ transcribe.py
@@ -1,209 +1,131 @@
-import whisper
-import torch
+
+import os
+import whisperx
+import json
+import numpy as np
+import string
+import torchaudio
 from whisperx.vads import Silero
-from pyannote.core import  Segment
-from transformers import WhisperTokenizer
-from difflib import SequenceMatcher
-import statistics
 
-from settings import *
-from model import *
-from dataset import annotation2tensor
-from decoding import *
+SAMPLE_RATE = 16000
+cache_dir = "cache"
+os.makedirs(cache_dir, exist_ok=True)
+audio_root = "/local/scratch/mli526/dataset/DW/audio"
 
-tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-base", language="en", task="transcribe")
-vad_options = {
-        "chunk_size": 30, # needed by silero since binarization happens before merge_chunks
-        "vad_onset": 0.500,
-        "vad_offset": 0.363
+output_folder = "/local/scratch/mli526/dataset/DW/whisper"
+device = "cuda"
+
+batch_size = 16 # reduce if low on GPU mem
+compute_type = "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
+
+asr_options = {
+    "initial_prompt": "Hola, ¿cómo estás? We're switching between English y español en esta conversación. Sometimes hablamos en inglés, and other times cambiamos al español. ",
+    #"condition_on_previous_text": True,
 }
-vad_model = Silero(**vad_options)
+vad_options = {
+        "chunk_size": 15, # needed by silero since binarization happens before merge_chunks
+        "vad_onset": 0.5,
+        "vad_offset": 0.36
+}
+model = whisperx.load_model("large-v3", device, compute_type=compute_type, vad_method="silero", asr_options=asr_options, vad_options=vad_options)
+model_a, metadata = whisperx.load_align_model(language_code="en", device=device)
+#vad = Silero(**vad_options)
+os.makedirs(output_folder, exist_ok=True)
 
-def get_segments(lines):
-    result = []
-    for line in lines:
-        if len(line) < 8:
+for root, dirs, files in os.walk(audio_root):
+    output_root = root.replace(audio_root, output_folder)
+    for dir in dirs:
+        output_dir = os.path.join(output_root, dir)
+        os.makedirs(output_dir, exist_ok=True)
+    for file in files:
+        if file[-3:] != 'mp3' and file[-3:] != 'wav':
             continue
-        spk = line[-2]
-        content = line[:-7]
-        result.append({"speaker":spk, "text": content})
-    return result
+        #if file[:-4] not in ["DW_A0013", "DW_A0024"]:
+        #    continue
+        output_path = os.path.join(output_root, file[:-3]+"txt")
+        audio_path = os.path.join(root, file)
+        print(audio_path)
+        #ori_audio, sr = torchaudio.load(audio_path)
+        # channel1 = ori_audio[0,:].unsqueeze(0)
+        # channel1_path = os.path.join(cache_dir, file[:-4]+"_1.mp3")
+        # torchaudio.save(channel1_path, channel1, sr)
+        audio = whisperx.load_audio(audio_path)
+        #print(audio.shape)
+        all_results = []
+        
+        # segments = vad({"waveform": audio, "sample_rate": SAMPLE_RATE})
+        # curr_start = 0
+        # curr_end = 0
+        # curr_lang = None
+ 
+        # for segment in segments:
+        #     start_frame = int(segment.start * SAMPLE_RATE)
+        #     end_frame = int(segment.end * SAMPLE_RATE)
+        #     lan = model.detect_language(audio[start_frame:end_frame])
+        #     lan = lan if lan in ["es", "en"] and (segment.end-segment.start)>0.5 else curr_lang
+        #     if lan == curr_lang or curr_lang is None:
+        #         curr_end = segment.end
+        #     else:
+        #         start_frame = int(curr_start * SAMPLE_RATE)
+        #         end_frame = int(curr_end * SAMPLE_RATE)
+        #         print(curr_end-curr_start)
+        #         results = model.transcribe(audio[start_frame:end_frame], batch_size=batch_size, language=curr_lang)
+        #         for result in results["segments"]:
+        #             result["start"] += curr_start
+        #             result["end"] += curr_start
+        #             all_results.append(result)
+        #         curr_start = segment.start
+        #         curr_end = segment.end
+        #     curr_lang = lan
+            
+        # start_frame = int(curr_start * SAMPLE_RATE)
+        # end_frame = int(curr_end * SAMPLE_RATE)
+        # results = model.transcribe(audio[start_frame:end_frame], batch_size=batch_size)
+        # for result in results["segments"]:
+        #     result["start"] += curr_start
+        #     result["end"] += curr_start
+        #     all_results.append(result)
+      
+        #span_segments = vad.merge_chunks(spanish_chunks, 15)
+        #eng_segments = vad.merge_chunks(english_chunks, 15)
+        # for segment in span_segments:
+        #     start_frame = int(segment["start"] * SAMPLE_RATE)
+        #     end_frame = int(segment["end"] * SAMPLE_RATE)
+        #     results = model.transcribe(audio[start_frame:end_frame], batch_size=batch_size)
+        #     all_results.extend(results["segments"])
+            
+        # for segment in eng_segments:
+        #     start_frame = int(segment["start"] * SAMPLE_RATE)
+        #     end_frame = int(segment["end"] * SAMPLE_RATE)
+        #     results = model.transcribe(audio[start_frame:end_frame], batch_size=batch_size)
+        #     all_results.extend(results["segments"])
+        results = model.transcribe(audio, batch_size=batch_size)
 
-
-def transcribe_segment(waveform, start, end, annotation, speakers, model, device):
-    start_frame = int(start * SAMPLE_RATE)
-    end_frame = int(end * SAMPLE_RATE)
-    audio = waveform[start_frame:end_frame]
-    audio = whisper.pad_or_trim(audio, CHUNK_LENGTH * SAMPLE_RATE)
-    mel = whisper.log_mel_spectrogram(audio)
-    segment_annotation = annotation.crop(Segment(start, end))
-    segment_annotation = annotation2tensor(segment_annotation, speakers, start)
+        results = whisperx.align(results["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+        with open(output_path, "w") as f:
+            for segment in results["segments"]:
+                start = segment["start"]
+                end = segment["end"]
+                text = segment["text"]
+                f.write(f"{start:.2f}\t{end:.2f}\t{text}\n")
+        #with open(output_json_path, "w") as f:
+            #json.dump(results, f)
     
-    tokens = tokenizer.encode("")[:-1]
-    with torch.no_grad():
-        mel = mel.to(device).unsqueeze(0)
-        segment_annotation = segment_annotation.to(device).unsqueeze(0)
-        audio_features = model.embed_audio(mel, segment_annotation)
-        candidates = beam_search_decode(model, audio_features, tokens, 50257, 8)
-        tokens, _ = candidates[0]
-    text = tokenizer.decode(tokens[4:-1])
-    #print(text)
-    result = get_segments(text.split("\n"))
-    return result
 
-def transcribe(waveform, annotation, model, device):
-    segments = vad_model({"waveform": waveform, "sample_rate": SAMPLE_RATE})
-    segments = vad_model.merge_chunks(segments, CHUNK_LENGTH)
-    speakers = {}
-    result = []
-    for segment in segments:
-        tmp = transcribe_segment(waveform, segment["start"], segment["end"], annotation, speakers, model, device)
-        result.extend(tmp)
-    return result
-
-def align(seq1, seq2):
-    m, n = len(seq1), len(seq2)
-    dp = np.zeros((m+1, n+1), dtype=float)
-
-    for j in range(1, n+1):
-        dp[0][j] =  j
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            cost = 1 - SequenceMatcher(None, seq1[i-1], seq2[j-1]).ratio()
-            dp[i][j] = min(
-                dp[i-1][j] + 1,       # deletion
-                dp[i][j-1] + 1,       # insertion
-                dp[i-1][j-1] + min(1, 2 * cost)   # substitution
-            )
-   
-    aligned = []
-    i, j = len(seq1), len(seq2)
-    while i > 0 or j > 0:
-        if i > 0 and j > 0:
-            if dp[i-1][j] < min(dp[i][j-1], dp[i-1][j-1]):
-                aligned.append((i-1, None))
-                i -= 1
-            elif dp[i][j-1] < min(dp[i-1][j], dp[i-1][j-1]):
-                aligned.append((None, j-1))
-                j -= 1
-            else:
-                aligned.append((i-1, j-1))
-                i -= 1
-                j -= 1
-        elif i <= 0:
-            aligned.append((None, j-1))
-            j -= 1
-        else:
-            aligned.append((i-1, None))
-            i -= 1
-    aligned.reverse()
-    return aligned
-
-def aggregate(overlapping_result):
-    candidates = []
-    for segment in overlapping_result[0]["window"]:
-        candidates.append({"candidates_spk":[segment["speaker"]], "text":segment["text"]})
-    start_idx = 0
-    cur_end = overlapping_result[0]["end"]
+        # for segment in output_segments:
+        #     segment["speaker"] = "1"
+        # channel2 = ori_audio[1,:].unsqueeze(0)
+        # channel2_path = os.path.join(cache_dir, file[:-4]+"_2.mp3")
+        # torchaudio.save(channel2_path, channel2, sr)
+        # audio = whisperx.load_audio(channel2_path)
+        # results = model.transcribe(audio, batch_size=batch_size)
+        # output_segments2 = whisperx.align(results["segments"], model_a, metadata, audio, device, return_char_alignments=False)["segments"]
+        # for segment in output_segments2:
+        #     segment["speaker"] = "2"
+        # output_segments.extend(output_segments2)
+        # output_segments.sort(key=lambda x:x["start"])
     
-    for window in overlapping_result:
 
-        if window["start"] >= cur_end:
-            cur_end = window["end"]
-            start_idx = len(candidates)
-            for segment in window["window"]:
-                candidates.append({"candidates_spk":[segment["speaker"]], "text":segment["text"]})
-                
-        else:
-            pre_tokens = []
-            pre_tokens_idx = []
-            segment_len = []
-            max_offset = len(candidates)
-            for offset in range(start_idx, max_offset):
-                tokens = candidates[offset]["text"].split(" ")
-                pre_tokens.extend(tokens)
-                segment_len.append(len(tokens))
-                for _ in tokens:
-                    pre_tokens_idx.append(offset)
-            cur_tokens = []
-            cur_tokens_idx = []
-            for idx, segment in enumerate(window["window"]):
-                tokens = segment["text"].split(" ")
-                cur_tokens.extend(tokens)
-                for _ in tokens:
-                    cur_tokens_idx.append(idx)
-                
-            align_result = align(pre_tokens, cur_tokens)
-          
-            speakers_stat = [[] for _ in range(start_idx, max_offset)]
-            for i, j in align_result:
-                if i is None or j is None:
-                    continue
-                offset = pre_tokens_idx[i]
-                spk = window["window"][cur_tokens_idx[j]]["speaker"]
-                speakers_stat[offset-start_idx].append(spk)
-            for offset in range(start_idx, max_offset):
-                if speakers_stat[offset-start_idx] == []:
-                    continue
-                spk = statistics.mode(speakers_stat[offset-start_idx])
-                if speakers_stat[offset-start_idx].count(spk) >= 0.5 * segment_len[offset-start_idx]:
-                    candidates[offset]["candidates_spk"].append(spk)
-            
-            
-            for i, j in align_result:
-                if j is not None and i is not None:
-                    break
-            if i is not None:
-                
-                start_idx = min(pre_tokens_idx[i], start_idx+1)
-                
-                    
-            pre_j = None
-            
-            for i, j in reversed(align_result):
-                if i is not None:
-                    break
-                pre_j = j
-            if pre_j is not None and pre_j < len(cur_tokens)-1:
-                extra_start = pre_j
-                cur_end = window["end"]
-            else:
-                extra_start = len(cur_tokens)
-
-            new_sgement = {"text":""}
-            for i in range(extra_start, len(cur_tokens)):
-                if cur_tokens[i] == "":
-                    continue
-                new_sgement["text"] += cur_tokens[i]
-                new_sgement["candidates_spk"] = [window["window"][cur_tokens_idx[i]]["speaker"]]
-                
-                if cur_tokens[i][-1] in [".", "?"]:
-                    candidates.append(new_sgement)
-                    new_sgement = {"text":""}
-                else:
-                    new_sgement["text"] += " "
 
         
-    result = []
-    for segment in candidates:
-        spk = statistics.mode(segment["candidates_spk"])
-        result.append({"speaker":spk, "text":segment["text"]})
-        
-    return result
-            
 
-def transcribe_overlap_voting(waveform, annotation, model, device):
-    segments = vad_model({"waveform": waveform, "sample_rate": SAMPLE_RATE})
-    speakers = {}
-    overlapping_result = []
-    for i in range(len(segments)):
-        j = i+1
-        start = segments[i].start
-        end = segments[i].end
-        while j < len(segments) and segments[j].end - start < CHUNK_LENGTH:
-            end = segments[j].end
-            j += 1
-        window = transcribe_segment(waveform, start, end, annotation, speakers, model, device)
-        overlapping_result.append({"window":window, "start":start, "end":end})
-    
-    return aggregate(overlapping_result)
